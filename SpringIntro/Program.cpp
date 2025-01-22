9d7112596caf5e6cb778d583d737bed05a3e83bf
Dallan Simper <dallan.simper@gmail.com>
2025-01-22 14:50:03 -0700

    cin

--- SpringIntro/Program.cpp
+++ SpringIntro/Program.cpp
@@ -4,14 +4,19 @@ using namespace std;
 
 
 int main() {
-	
 
-	int hens = 14;
-	int eggs = 2;
 
-	double percent = (eggs* 100.0) / hens;
+	int hens;
+	int eggs;
 
-	cout << percent << "%";
+	cout << "How many hens do you have? ";
+	cin >> hens;
+	cout << "How many eggs did you get today? ";
+	cin >> eggs;
+
+	double percent = (eggs * 100.0) / hens;
+
+	cout  << percent << "% of you hens laid eggs.";
 
 
 	return 0;
