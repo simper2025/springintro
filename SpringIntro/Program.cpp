867847da72320eee0d6af8b768c05926538a17ff
Dallan Simper <dallan.simper@gmail.com>
2025-01-27 15:32:03 -0700

    hit strength

--- SpringIntro/Program.cpp
+++ SpringIntro/Program.cpp
@@ -5,29 +5,14 @@ using namespace std;
 
 int main() {
 
-	int cents;
-	cout << "How many cents change? ";
-	cin >> cents;
+	int hitpoint = 30;
+	int damage;
+	cout << "The monster has " << hitpoint << " hitpoints." 
+		<< endl;
+	cout << "How much damage do you do? ";
+	cin >> damage;
 
-	int quarters = cents / 25;
-	if (quarters > 0) {
-		cout << "Give them " << quarters << " quarters." << endl;
-	}
-	cents = cents % 25;
 
-	int dimes = cents / 10;
-	if (dimes > 0) {
-		cout << "Give them " << dimes << " dimes." << endl;
-	}
-	cents = cents % 10;
-
-	int nickels = cents / 5;
-	if (nickels > 0) {
-		cout << "Give them " << nickels << " nickels." << endl;
-	}
-	cents = cents % 5;
-
-	cout << "Give them " << cents << " pennies." << endl;
 
 	return 0;
 }
