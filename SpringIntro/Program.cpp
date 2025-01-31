52f4fd897c0a05e8e289960c53a380140358bb0b
Dallan Simper <dallan.simper@gmail.com>
2025-01-31 15:46:41 -0700

    first menu

--- SpringIntro/Program.cpp
+++ SpringIntro/Program.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 
 int main() {
 
-	int num = 0;
+	string input = "a";
 
-	while (true) {
-		if (num % 10000 == 0)
-			cout << num << endl;
-		num = num + 765431;
+	while (input != "q") {
+		cout << "Here is my menu \n";
+		cout << "1 - Count to ten\n";
+		cout << "q - quit\n";
+		cout << "What do you want to do? ";
+		cin >> input;
+
+		cout << endl;
+
+		if (input == "1") {
+			int num = 1;
+			while (num <= 10) {
+				cout << num << endl;
+				num++;
+			}
+		}
+		else if (input == "q") {
+
+		}
+		else {
+			cout << "That is not a menu option.\n";
+		}
 	}
 
 	return 0;
