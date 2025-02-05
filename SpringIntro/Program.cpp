ca2929ad101b3db2a4f1e368152bba00e3e514db
Dallan Simper <dallan.simper@gmail.com>
2025-02-05 15:19:31 -0700

    functions

--- SpringIntro/Program.cpp
+++ SpringIntro/Program.cpp
@@ -1,29 +1,60 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 
 
+
+
+string Letter(string name);
+
+void MakeLetter();
+
+int Sum(int, int);
+
 int main() {
 	
-	/*char c = 61;
-	cout << "Char: " << c << endl;*/
+	cout << Sum(5, 6) <<endl;
 
-	int count = 0;
-	int i;
-	cout << "Start Position: ";
-	cin >> i;
-	while (i != 1) {
-		if (i % 2 == 1)
-			i = i * 3 + 1;
-		else
-			i = i / 2;
-		cout << "Value: " << i << endl;
-		count++;
-	}
-	cout << "Completed: " << count;
+	int num1 = 45;
+	int num2 = -37;
+
+	cout << Sum(num1, num2) << endl;
+
+	MakeLetter();
+
+	int num = 62;
+	char c = (char)num;
+	cout << c;
+
 
 	return 0;
 }
+
+
+int Sum(int a, int b)
+{
+	return a + b;
+}
+
+
+string Letter(string name) {
+	return "Dear " + name + ", \nI am writing to inform you that you owe " +
+		"the sum of $20. \n\nSincerly,\nYour Grandmother";
+}
+
+
+
+void MakeLetter() {
+	string name;
+	cout << "Please enter your name: ";
+	cin >> name;
+
+	string letter = Letter(name);
+	cout << letter << endl;
+}
+
+
 
 
