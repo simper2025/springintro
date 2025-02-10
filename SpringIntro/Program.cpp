1bcb39fc1c96d97f68d9e1f168ef341841d789a4
Dallan Simper <dallan.simper@gmail.com>
2025-02-10 15:11:20 -0700

    arrays and for

--- SpringIntro/Program.cpp
+++ SpringIntro/Program.cpp
@@ -3,20 +3,22 @@
 using namespace std;
 
 int main() {
+	const int DAYS = 31;
+	int eggs[DAYS];
 
-
-	for (int i = 0; i <= 256; i++) {
-		if (i != 27) {
-			cout //<< i << " "
-				<< (char)i << " ";
-		}
-		else
-		{
-			cout << "  ";
-		}
-		if (i % 16 == 0)
-			cout << endl;
+	for (int i = 0; i < DAYS; i++)
+	{
+		cout << "How eggs did the chickens lay on day " << i+1 << ": ";
+		cin >> eggs[i];
 	}
+
+	int total = 0;
+	for (int i = 0; i < DAYS; i++)
+	{
+		total = total + eggs[i];
+	}
+	
+	cout << "The chickens laid " << total << " eggs.\n";
 
 	return 0;
 }
