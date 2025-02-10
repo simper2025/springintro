86f136291c7fe3074f6b84a644472a91d9df994a
Dallan Simper <dallan.simper@gmail.com>
2025-02-10 15:30:27 -0700

    parrelell arrays

--- SpringIntro/Program.cpp
+++ SpringIntro/Program.cpp
@@ -3,23 +3,28 @@
 using namespace std;
 
 int main() {
-	const int DAYS = 31;
-	int eggs[DAYS];
 
-	for (int i = 0; i < DAYS; i++)
+	int totals[] = { 1,2,3,4,5,12,3,45,6,7 };
+	int scores[10];
+
+	for (int i = 0; i < 10; i++)
 	{
-		cout << "How eggs did the chickens lay on day " << i+1 << ": ";
-		cin >> eggs[i];
+		cout << "What grade did the student get for assignment #"
+			<< i + 1 << ", out of " << totals[i] << " points? ";
+		cin >>  scores[i];
 	}
 
-	int total = 0;
-	for (int i = 0; i < DAYS; i++)
+	double totalPoints = 0.0;
+	double studentTotal = 0.0;
+	for (int i = 0; i < 10; i++)
 	{
-		total = total + eggs[i];
+		cout << "For assignment #" << i + 1 << " the student got " 
+			<< ((double)scores[i]/ totals[i]) * 100.0 << "%.\n";
+		totalPoints += totals[i];
+		studentTotal += scores[i];
 	}
+	cout << "The students overall score is " << studentTotal / totalPoints * 100.0 << "%.\n";
 	
-	cout << "The chickens laid " << total << " eggs.\n";
-
 	return 0;
 }
 
