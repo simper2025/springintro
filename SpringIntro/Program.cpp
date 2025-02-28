c52f15f421b4878e50610dbfa2c68229e5a7c8ae
Dallan Simper <dallan.simper@gmail.com>
2025-02-28 15:18:48 -0700

    sorting

--- SpringIntro/Program.cpp
+++ SpringIntro/Program.cpp
@@ -1,20 +1,49 @@
 #include <iostream>
-
 using namespace std;
 
-
+void printArray(int prefix, int a[], int size);
 
 int main() {
-	
-	srand(time(0));
 
-	int num = rand() % 6 + 15;
-	cout << num << endl;
+	const int SIZE = 13;
+	int arr[SIZE] = { 21,4,6,14,8,43,9,2,1,3,56,7,22 };
+	//int arr[SIZE] = { 13,12,11,10,9,8,7,6,5,4,3,2,1 };
+
+	printArray(-1, arr, SIZE);
+
+	for (int j = 0; j < SIZE - 1; j++) {
+		bool sorted = true;
+
+		for (int i = 0; i < SIZE - 1 - j; i++) {
+			if (arr[i] > arr[i + 1]) {
+				int temp = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = temp;
+				sorted = false;
+			}
+			break;
+		}
+		printArray(j, arr, SIZE);
+		if (sorted) {
+			break;
+		}
+	}
+
 
 	return 0;
+
+
 }
 
 
+
+void printArray(int prefix, int a[], int size) {
+	cout << "#" << prefix << ": ";
+	for (int i = 0; i < size; i++) {
+		cout << a[i] << ", ";
+	}
+	cout << endl;
+}
 
 
 
