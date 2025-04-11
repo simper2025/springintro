c8339d855f63a864ee14e7a5f6b25b553a06f57c
Dallan Simper <dallan.simper@gmail.com>
2025-04-11 15:05:35 -0600

    start of pointers

--- SpringIntro/Program.cpp
+++ SpringIntro/Program.cpp
@@ -4,20 +4,29 @@
 
 using namespace std;
 
-void Power(int num, int exp, int &answer) {
-	answer = 1;
-
+void Power(int num, int exp, int *answer) {
+	*answer = 1;
+	int value = 27;
 	for (int i = 0; i < exp; i++)
 	{
-		answer *= num;
+		*answer *= num;
 	}
 }
 
 int main() {
 	int a = 3;
 
-	int value;
-	Power(a, 4, value);
+	int* ptr = nullptr;
+	int value = 0;
+	ptr = &value;
+
+
+
+	cout << "value: " << value << ", &value: " << &value << endl;
+	cout << "ptr: " << ptr << ", &ptr: " << &ptr << " *ptr : " << *ptr << endl;
+
+
+	Power(a, 4, ptr);
 
 	cout << value << endl;
 
