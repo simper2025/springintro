cf6cba9977f736e5131b5d4981795244b90fe487
Dallan Simper <dallan.simper@gmail.com>
2025-04-15 08:55:49 -0600

    more pointers

--- SpringIntro/Program.cpp
+++ SpringIntro/Program.cpp
@@ -4,9 +4,24 @@
 
 using namespace std;
 
+void Add(int a, int b, int&);
+
+void Add(int a, int b, int &sum) {
+	sum = a + b;
+}
+
+int foo(int a, int& c, vector<vector<int>> * list) {
+	c = a + 7;
+	return 5;
+}
+
 int* foo() {
 	int* ptr1 = new int;
+	
 	*ptr1 = 72;
+	cout << "ptr1: " << ptr1 << endl;
+	cout << "*ptr1: " << *ptr1 << endl;
+	cout << "&ptr1: " << &ptr1 << endl;
 
 	int c = 13;
 	int* ptr2 = &c;
@@ -19,9 +34,28 @@ int* foo() {
 }
 
 
+bool IsValid(int a) {
+	return (a > 1);
+}
+
+
 int main() {
+
+	int a = 1;
+	int b = 2;
+	int c = 27;
+
+	Add(a, 3, c);
+	cout << "Sum: " << c << endl;
+	cout << "&a: " << &a << endl;
 	
+	foo();
+
 	int* p = new int [12];
+
+	if (IsValid(a)) {
+
+	}
 
 	*(p + 0) = 21;
 
